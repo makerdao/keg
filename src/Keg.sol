0d80d67e1ecf7dabbea64d337bb304f7200866fa
NiklasKunkel <Niklas@theKunkels.com>
2020-06-26 11:36:47 -0700

    updated pragma

--- src/Keg.sol
+++ src/Keg.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.12;
+pragma solidity >=0.5.15;
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
